2d470b5c6f78119e4a0600ff6ef06ff34f617553
Manuel-MV15 <mmelendez1505@gmail.com>
2024-06-22 18:07:43 -0600

    Conceccion a la base de datos

--- Barberia/obj/Debug/net8.0-windows/Barberia.AssemblyInfo.cs
+++ Barberia/obj/Debug/net8.0-windows/Barberia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barberia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa62fc4f0603371355afd7227af9774ae4b42908")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barberia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barberia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

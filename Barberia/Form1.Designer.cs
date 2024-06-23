7b674c80cb7ff971f6c7db4d5d90e3f881aeab99
Manuel-MV15 <mmelendez1505@gmail.com>
2024-06-22 19:35:11 -0600

    Inicio de secion

--- Barberia/Form1.Designer.cs
+++ Barberia/Form1.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txt_Usuario = new TextBox();
+            txt_Clave = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btn_Ingresar = new Button();
+            SuspendLayout();
+            // 
+            // txt_Usuario
+            // 
+            txt_Usuario.Location = new Point(295, 106);
+            txt_Usuario.Name = "txt_Usuario";
+            txt_Usuario.Size = new Size(250, 47);
+            txt_Usuario.TabIndex = 0;
+            // 
+            // txt_Clave
+            // 
+            txt_Clave.Location = new Point(295, 201);
+            txt_Clave.Name = "txt_Clave";
+            txt_Clave.Size = new Size(250, 47);
+            txt_Clave.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(104, 106);
+            label1.Name = "label1";
+            label1.Size = new Size(119, 41);
+            label1.TabIndex = 2;
+            label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(104, 201);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 41);
+            label2.TabIndex = 3;
+            label2.Text = "Clave";
+            // 
+            // btn_Ingresar
+            // 
+            btn_Ingresar.Location = new Point(244, 361);
+            btn_Ingresar.Name = "btn_Ingresar";
+            btn_Ingresar.Size = new Size(188, 58);
+            btn_Ingresar.TabIndex = 4;
+            btn_Ingresar.Text = "Ingresar";
+            btn_Ingresar.UseVisualStyleBackColor = true;
+            btn_Ingresar.Click += btn_Ingresar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(869, 615);
+            Controls.Add(btn_Ingresar);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txt_Clave);
+            Controls.Add(txt_Usuario);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txt_Usuario;
+        private TextBox txt_Clave;
+        private Label label1;
+        private Label label2;
+        private Button btn_Ingresar;
     }
 }
--- Barberia/Form1.cs
+++ Barberia/Form1.cs
@@ -1,3 +1,7 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using MySql.Data.MySqlClient;
+using System.Text;
+
 namespace Barberia
 {
     public partial class Form1 : Form
@@ -5,6 +9,36 @@ namespace Barberia
         public Form1()
         {
             InitializeComponent();
+
         }
+        private void btn_Ingresar_Click(object sender, EventArgs e)
+        {
+            Encriptador encriptador = new Encriptador();
+            // inicio de secion 
+            string Usuario = txt_Usuario.Text;
+            string Clave = txt_Clave.Text;
+
+            string consulta = $"SELECT * FROM tbl_usuarios WHERE Usuario = '{Usuario}' and Clave = '{encriptador.Encriptar(Clave)}';";
+            // coneccion a la base de datos 
+            MySqlConnection conexcion = Conexcion.MyConnection();
+            conexcion.Open();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(consulta, conexcion);
+                cmd.ExecuteNonQuery();
+
+                int dato = Convert.ToInt32(cmd.ExecuteScalar());
+                if (dato > 0)
+                {
+                    MessageBox.Show("Inicio exitoso");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex);
+            }
+        }
+
+
     }
 }
